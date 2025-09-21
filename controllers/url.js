c2889b9477ad2aff84c6b4e8161e6992bc9d8fa6
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-09-21 15:36:41 +0530

    Server side rendering: added frontend

--- controllers/url.js
+++ controllers/url.js
@@ -1,5 +1,6 @@
 const {nanoid} = require('nanoid')// nanoid generate unique IDs.
 const URL = require('../models/url')//here we have imported the database
+
 async function handleGenerateNewShortURL(req, res){
     const body = req.body;
     if(!body.url) return res.status(400).json({error:'url is required'})
@@ -10,11 +11,25 @@ async function handleGenerateNewShortURL(req, res){
         visitHistory: [],
     })
 
-    return res.json({id: shortID})
+    return res.render('home', {
+        id: shortID
+    })
 }
 async function handleGetAnalytics(req, res){
     const shortId=request.params.shortId
     const result=await URL.findOne({shortId})
     return res.json({totalClicks: result.visitHistory.length, analytics: result.visitHistory})
 }
-module.exports= {handleGenerateNewShortURL,handleGetAnalytics}
+
+async function handleRedirectToOriginalURL(req, res) {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOne({ shortId });
+    if (!entry) return res.status(404).send('URL not found');
+    // Optionally, update visitHistory here if you want to track visits
+    return res.redirect(entry.redirectURL);
+}
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytics,
+    handleRedirectToOriginalURL
+};
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const path =require('path')
 const {connectToMongoDB} = require('./connect.js')
 const urlRoute = require('./routes/url')
+const staticRoute = require('./routes/staticRouter.js')
 const URL = require('./models/url.js')
 const app = express()
 
@@ -9,10 +11,25 @@ const PORT = 5000
 connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(()=>console.log(`mongodb connected`))
 
+app.set("view engine", "ejs")
+app.set('views', path.resolve("./views"))
+
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 
 app.use('/url', urlRoute)
+app.use("/", staticRoute)
+
+app.get("/test", async(req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render('home', {
+        urls: allUrls,
+        name:'Akshita'
+    })
+})
+
 app.get('/:shortId', async(req, res)=> {
     const shortId = req.params.shortId;
     const entry=await URL.findByIdAndUpdate({
